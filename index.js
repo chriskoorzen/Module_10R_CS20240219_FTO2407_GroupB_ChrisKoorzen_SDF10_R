a9ae5dd7c3fa476580d1477ffdd4dfe3b3458847
Chris Koorzen <fckoorzen@gmail.com>
2024-07-31 16:11:43 +0200

    Terminate statements with semicolon, add 2 edge case handlers.

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    let inputValue = inputFieldEl.value;
     
-    console.log(inputValue)
-})
+    // -- Edge Cases --
+    // String is white space only, or has redundant white space.
+    inputValue = inputValue.trim();
+
+    // Finally, if empty or null string, do nothing.
+    if (inputValue === "" || inputValue === null){ return; }
+
+    // -- end of edge case handling --
+
+
+    console.log(inputValue);
+});
